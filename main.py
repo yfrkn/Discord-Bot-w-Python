8af788ccbcfc30fe3a0d0867a6de23a6386ce190
Furkan <113787132+yfrkn@users.noreply.github.com>
2023-04-19 21:47:28 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+#Create a bot from https://discord.com/developers/applications
+#Open your bot's Server Member and Message Content intents
+#Add your bot to your server from https://discordapi.com/permissions.html
+
+import discord
+from discord.ext import commands
+from ayarlar import *
+from functions import *
+
+intents = discord.Intents().all()
+intents.message_content = True
+Bot = commands.Bot(command_prefix='YOUR_BOT'S_PREFIX', intents=intents)
+
+#READY
+@Bot.event
+async def on_ready():
+    print("Bot is ready")
+
+#HELP
+@Bot.command()
+async def help(ctx, *args):
+    if "commands" in args:
+        await ctx.send("Your Command List")
+    else:
+        await ctx.send("Help For Your Bot") 
+
+#HELLO
+@Bot.command()
+async def hello(ctx):
+    await ctx.send("Hi")
+
+Bot.run("YOUR_BOT'S_TOKEN")
